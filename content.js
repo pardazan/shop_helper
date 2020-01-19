02915fd8772cf2dbfb841974a016cab55101f5bf
pardazan <pardazan@gmail.com>
2020-01-19 19:18:02 +0330

    Detect basket and get items

---
+++ content.js
@@ -0,0 +1,27 @@
+if (document.URL.indexOf("https://www.boyner.com.tr/sepet") >= 0) {
+    alert("شما در مرحله پایانی خرید هستید");
+    setTimeout(() => {
+        let tmpBasketItem = document.getElementsByClassName("o-basket__item");
+        console.log("Basket Item:");
+        console.log(tmpBasketItem);
+        /*  let paragraphs = document.getElementsByTagName('p');
+      
+               for (elt of paragraphs) {
+                  elt.style['background-color'] = '#CCCCCC';
+              }
+          const basketElement = document.querySelector('h1');
+          const basketText = basketElement.textContent;
+          console.log(basketText);
+          const basketItemNumber = basketText.replace(/\D/g, '');
+          for (i = 2; i <= parseInt(basketItemNumber) + 1; i++) {
+              const basketitemsPriceFound = document.querySelector('div:nth-of-type(' + i + ') .o-productPrice ins');
+              console.log('div:nth-of-type(' + i + ') .o-productPrice ins');
+              console.log(basketitemsPriceFound.textContent);
+          }
+      
+          const totalPay = document.querySelector('.-total div.o-orderSummary__itemVal');
+          let totalPayText = totalPay.textContent;
+          alert('Your final total payment is  ' + totalPayText + '\n' + 'You have choosen '
+              + basketItemNumber + ' Number of products!'); */
+    }, 5000);
+}
