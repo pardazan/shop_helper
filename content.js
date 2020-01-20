c2f5bf2ccc551838bbc7f2ed6303717f7e3c0562
pardazan <pardazan@gmail.com>
2020-01-20 17:36:33 +0330

    Remove unused (test) comments

--- content.js
+++ content.js
@@ -16,12 +16,7 @@ if (document.URL.indexOf("https://www.boyner.com.tr/sepet") >= 0) {
             console.log("Price[" + i + "] : " + tmpBasketPrice[i].innerText)
             console.log("Price HTML[" + i + "] : " + tmpBasketPrice[i].innerHTML)
         }
-/*
-this is just a test. (Added by Akbar-P)
-This new line is added by Pardazan
-Wait to add 3rd line? here it is!!
-waiting is enough 4th Line By Akbar
-*/
+
         /*  let paragraphs = document.getElementsByTagName('p');
       
                for (elt of paragraphs) {
